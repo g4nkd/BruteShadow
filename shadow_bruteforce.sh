c857fd606c5dbdf64802bdbffaea8f58fd15608f
g4nkd <150204934+g4nkd@users.noreply.github.com>
2024-01-30 19:46:28 -0300

    v0.1

---
+++ shadow_bruteforce.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+hash_verdadeira=$3
+
+echo "Types of hash:"
+echo "1 - MD5"
+echo "2a - Blowfish"
+echo "5 - SHA-256"
+echo "6 - SHA-512
+"
+
+if [ $# -ne 3 ]; then
+    echo "Usage mode: $0 <wordlist> <salt> <hash> <tipo>"
+    exit 1
+fi
+
+if [ ! -e "$1" ]; then
+    echo "Wordlist file not found: $1"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    passwd=$(openssl passwd $4 -salt "$2" "$line" | cut -d "$" -f4)
+
+    if [ "$passwd" == "$hash_verdadeira" ]; then
+        echo "Password: $line"
+        exit 0
+    else
+        echo "Trying: $line"
+    fi
+done < "$1"
+
+echo "Password not found :("
+exit 1
+
